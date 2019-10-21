5a7e12ee2320db3516f0384bfd8b09bf758b7566
Matthew Pelletier <matthewbpelletier@gmail.com>
2019-10-21 16:13:41 -0400

    Started script pseudocode

---
+++ deleteLocalUser/scripts/disable-local-user.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Must be executed with superuser privileges. Return 1. Display on stderr
+
+
+#Check for args; supply usage statement if no username supplied; exit 1. Display on stderr
+
+
+#Perform actions for each username
+
+
+#Skip accounts with UID < 1000
+
+
+#Provide message for end result
