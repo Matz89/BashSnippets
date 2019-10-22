8bf09ad25b04c5145bd80d72fd38e1dcccd5fa18
Matthew Pelletier <matthewbpelletier@gmail.com>
2019-10-21 20:54:55 -0400

    Merging Commit

--- deleteLocalUser/scripts/disable-local-user.sh
+++ deleteLocalUser/scripts/disable-local-user.sh
@@ -59,7 +59,7 @@ do
 	shift
 
 	#Check if user exists
-	USER_ID=$(id -u ${USERNAME} /dev/null 1>&2) 
+	USER_ID=$(id -u ${USERNAME}) 
 	if [[ ${?} -ne 0 ]]
 	then
 		echo "FAILURE: ${USERNAME} does not exist."
